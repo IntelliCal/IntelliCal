9a04a56edb7ebf9f252fc1787d2ba42737424bbb
Kevin Kim <kevink007@gmail.com>
2016-10-20 20:17:25 -0400

    Got hello world server

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 var port = process.env.PORT || 1337;
 
-app.set('views', __dirname + '/fronend/views');
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.use(express.static(__dirname + '/frontend/views'));
-app.use(bodyParser.json())
+// app.set('views', __dirname + '/fronend/views');
+// app.use(express.static(__dirname + '/frontend/views'));
+// app.use(bodyParser.json())
 
+app.get('/', function(req, res, next) {
+  res.send('Hello World!');
+});
 
 app.listen(port, function(){
   console.log('Hermes is listening on port: ', port);
