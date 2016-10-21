a19ecb7d44ddef0b166a7d7dfe7326356d2907d5
Kevin Kim <kevink007@gmail.com>
2016-10-20 20:53:37 -0400

    Added index.html routing

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var app = express();
+
+var morgan = require('morgan');
+var path = require ('path');
 var bodyParser = require('body-parser');
+
+
 var port = process.env.PORT || 1337;
 
-// app.set('views', __dirname + '/fronend/views');
-// app.use(express.static(__dirname + '/frontend/views'));
-// app.use(bodyParser.json())
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/test', express.static(path.join(__dirname + '/../public')));
+app.use('/test/node_modules', express.static(path.join(__dirname + '/../node_modules')));
 
 app.get('/', function(req, res, next) {
-  res.send('Hello World!');
+  res.send('Hello Everyone');
 });
+
+
 
 app.listen(port, function(){
   console.log('Hermes is listening on port: ', port);
