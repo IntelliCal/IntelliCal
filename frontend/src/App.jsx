6a3fd28d52897672acb18c198762a6c4a1362ef1
Erica Fanelle <efanelle@gmail.com>
2016-10-21 22:40:44 -0400

    Hello world version of react component working

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
+    // };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
+    // <div>
+    //   <Monday />
+    //   </div>
+    //   <div>
+    //   <Tuesday />
+    //   </div>
+    //   <div>
+    //   <Wednesday />
+    //   </div>
+    //   <div>
+    //   <Thursday />
+    //   </div>
+    //   <div>
+    //   <Friday />
+    //   </div>
+    //   <div>
+    //   <Saturday />
+    //   </div>
