5e1529d2cb9885c5af3361163a6ac2adb1339f1b
lhawes <doubletigerstripe@gmail.com>
2016-10-24 10:02:55 -0700

    feat / Adding db functionality. created models to hold models. created readme for database schema/structure/info. created database.js for testing that the files work.

---
+++ backend/db/database.js
@@ -0,0 +1,20 @@
+var models = require("./models");
+
+// models.users.sync({force:true}).success(function(err){
+//   console.error(err);
+// });
+// models.tasks.sync({force:true}).success(function(err){
+//   console.error(err);
+// });
+
+// model.users.hasMany(Task);
+// Task.belongsTo(User);
+
+models.users.hasMany(models.tasks);
+models.tasks.belongsTo(models.users);
+
+
+models.users.all().then(function(taskList) {
+    console.log(taskList);
+    console.log('test');
+  });
