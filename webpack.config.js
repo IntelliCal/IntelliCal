c88ebe720831f45e278ffaca4e22adc386933996
lhawes <doubletigerstripe@gmail.com>
2016-10-25 15:06:52 -0700

    Adding in webpack functionality.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,62 @@
+// module.exports = {
+//   entry: './public/main.js',
+//   output: {
+//     path: './public',
+//     filename: 'bundle.js'
+//   },
+//   devServer: {
+//     inline: true,
+//     contentBase: './public',
+//     port: process.env.PORT || 8100
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         loader: 'babel',
+//         query:
+//           {
+//             presets:['react']
+//           }
+//       }
+//     ]
+//   }
+// }
+
+
+
+
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
 module.exports = {
-  entry: './public/main.js',
-  output: {
-    path: './public',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    inline: true,
-    contentBase: './public',
-    port: process.env.PORT || 8100
-  },
+  context: path.join(__dirname, "public"),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: './main.js',
   module: {
     loaders: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel',
-        query:
-          {
-            presets:['react']
-          }
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+          // ,
+          // plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties']
+        }
       }
     ]
-  }
-}
+  },
+  output: {
+    // path: __dirname + "/src/",
+    path: './public',
+    filename: "build.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
