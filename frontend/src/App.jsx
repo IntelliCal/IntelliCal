7fe153d0cb805b87c5b6a3d58e8f4d01397497da
Erica Fanelle <efanelle@gmail.com>
2016-10-24 21:17:38 -0400

    calendar rendering correctly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ function Month(month, year, dates){
   this.NumberOfDays = this.date.getDate();
   this.nameofmonth = months[this.date.getMonth()];
   this.firstday = 1;
+  this.year = this.date.getFullYear();
   this.calendar = generateCalendar(this.NumberOfDays, month-1, this.firstday, year, dates);
   console.log('this.calendar======', this.calendar)
 }
@@ -16,10 +17,21 @@ function dateToDay(year, month, day) {
 
 function generateCalendar(NumberOfDays, month, day, year, dates) {
   var weekday = dayOfWeek[dateToDay(year, month, day)];
+
   if (weekday in dates) {
+    // console.log('weekday and day', weekday, day)
     dates[weekday].push(day);
   } else {
-    dates[weekday] = [day];
+    console.log('weekday and day', weekday, day)
+      if (day === 1) {
+        var firstIndex = dayOfWeek.indexOf(weekday)
+        for (var i =0; i < firstIndex; i++) {
+          dates[dayOfWeek[i]] = ['-'];
+        }
+        dates[dayOfWeek[firstIndex]] = [1]
+      } else {
+        dates[weekday] = [day];
+      }
   }
   day++;
   return day > NumberOfDays ? dates : generateCalendar(NumberOfDays, month, day, year, dates);
@@ -36,6 +48,7 @@ class App extends React.Component {
   generateCalendar() {
     var month =  {};
     month = new Month(10, 2016, month);
+    console.log(month)
     return month;
   }
 
@@ -49,14 +62,14 @@ class App extends React.Component {
     return (
       <div id="calendar">
         <div id="header">
-        <p>October 2016</p>
+        <p>{this.state.dates.nameofmonth} {this.state.dates.year}</p>
         </div>
 
         <div id="dates">
           {calendar.map(function(week, i) {
             return (
-              <div>
-                <p className="weekName">{i}</p>
+              <div className="nopaddingmargin">
+                <p className="weekName">{dayOfWeek[i]}</p>
                 <ul className="nopaddingmargin">
                 {week.map(function(day, ind) {
                   return <li> {day} </li>
