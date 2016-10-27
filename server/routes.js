7c2c6b9f71d5fddc9945a7ce91d1a75cd0685a31
lhawes <doubletigerstripe@gmail.com>
2016-10-26 18:13:44 -0700

    (refactor) Refactored routes to be DRY-er and use database.js promises.

--- server/routes.js
+++ server/routes.js
@@ -1,77 +1,64 @@
-var tasks = require('../db/functions/tasksFunctions.js');
-var users = require('../db/functions/usersFunctions.js');
-var relational = require('../db/functions/relationalFunctions.js');
+var db = require('../db/database.js');
 
 // router for rest api
 module.exports = function(app, express) {
   var router = express.Router();
 
+  //middleware to keep routes DRY-er
+  var responseHandling = function (req, res, fn) {
+    fn.then(function (response) {
+        res.json(response);
+      })
+      .error(function (error) {
+        console.error(error);
+        res.json(error);
+      })
+  }
+
   //tasks
   router.route('/tasks')
     .get(function(req, res) {
-      tasks.getAllTasks(function(data) {
-        res.json(data);
-      })
+      responseHandling(req, res, db.getAllTasks());
     })
     .post(function(req, res) {
-      tasks.addTask(req.body, function(data) {
-        res.json(data);
-      })
+      responseHandling(req, res, db.addTask(req.body));
     });
 
   router.route('/tasks/:task_id')
     .get(function(req, res) {
-      tasks.getATask(req.params.task_id, function(data) {
-        res.json(data);
-      })
+      responseHandling(req, res, db.getATask(req.params.task_id));
     })
     .delete(function(req, res) {
-      tasks.deleteATask(req.params.task_id, function(data) {
-        res.json(data);
-      })
+      responseHandling(req, res, db.deleteATask(req.params.task_id));
     })
     .put(function(req, res) {
-      tasks.updateATask(req.params.task_id, req.body, function(data) {
-        res.json(data);
-      })
+      responseHandling(req, res, db.updateATask(req.params.task_id));
     });
 
   //users
   router.route('/users')
     .get(function(req, res) {
-      users.getAllUsers(function(data) {
-        res.json(data);
-      })
+      responseHandling(req, res, db.getAllUsers());
     })
     .post(function(req, res) {
-      users.addUser(req.body, function(data) {
-        res.json(data);
-      })
+      responseHandling(req, res, db.addUser(req.body));
     });
 
   router.route('/users/:user_id')
     .get(function(req, res) {
-      users.getAUser(req.params.user_id, function(data) {
-        res.json(data);
-      })
+      responseHandling(req, res, db.getAUser(req.params.user_id));
     })
     .delete(function(req, res) {
-      users.deleteAUser(req.params.user_id, function(data) {
-        res.json(data);
-      })
+      responseHandling(req, res, db.deleteAUser(req.params.user_id));
     })
     .put(function(req, res) {
-      users.updateAUser(req.params.user_id, req.body, function(data) {
-        res.json(data);
-      })
+      responseHandling(req, res, db.updateAUser(req.params.user_id, req.body));
     });
 
   //tasks for a specific given user
   router.route('/usertasks/:user_id')
     .get(function(req, res) {
-      relational.getAllTasksForAUser(req.params.user_id, function(data) {
-        res.json(data);
-      })
+      responseHandling(req, res, db.getAllTasksForAUser(req.params.user_id));
     })
 
   //test function to make sure api is working
