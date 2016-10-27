60eddaa0693f7089cf0f30a48268683d1afcfc3a
PHDorin <hanshirosan@gmail.com>
2016-10-27 10:12:06 -0600

    Worked on editable taks

---
+++ public/src/customTask.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import injectTapEventPlugin from 'react-tap-event-plugin';
+// import TextField from 'material-ui/TextField';
+// import FloatingActionButton from 'material-ui/FloatingActionButton';
+// import ContentAdd from 'material-ui/svg-icons/content/add';
+// injectTapEventPlugin();
+
+class CustomTask extends React.Component {
+  constructor(props){
+    super(props)
+    this.tasks = [];
+    this.currentId=1;
+    this.state ={
+      title:'',
+      description:'',
+      startTime:'',
+      endTime:'',
+    }
+    this.handleChange1 = this.handleChange1.bind(this)
+    this.handleChange2 = this.handleChange2.bind(this)
+    this.handleChange3 = this.handleChange3.bind(this)
+    this.handleChange4 = this.handleChange4.bind(this)
+    this.addTask = this.addTask.bind(this)
+  }
+
+  handleChange1(event){
+    this.setState({title: event.target.value})
+    console.log('in the change event', this.state.title)
+  }
+
+  handleChange2(event){
+    this.setState({description: event.target.value})
+  }
+
+  handleChange3(event){
+    this.setState({startTime: event.target.value})
+  }
+
+  handleChange4(event){
+    this.setState({endTime: event.target.value})
+  }
+
+  addTask(event){
+    var task = {
+      id: this.currentId,
+      title: this.state.title,
+      description: this.state.description,
+      startTime: this.state.startTime,
+      endTime: this.state.endTime
+    }
+    this.currentId ++
+    alert('field value is', this.state.title)
+    this.tasks.push(task)
+  }
+
+  render () {
+    return (
+      <div className='inputForm'>
+        <form>
+          <h2> Add a Task </h2>
+          Title:
+          <input
+            type='text'
+            required={true}
+            placeholder='Title'
+            value={this.state.title}
+            onChange={this.handleChange1}
+          /><br/> 
+          Description:
+          <input
+            type='text'
+            required={false}
+            value={this.state.description}
+            onChange={this.handleChange2}
+          /><br />
+          Start Time:   
+          <input
+            type='text'
+            required={true}
+            value={this.state.startTime}
+            onChange={this.handleChange3}
+          /><br /> 
+          End Time:  
+          <input
+            type='text'
+            required={true}
+            value={this.state.endTime}
+            onChange={this.handleChange4}
+          /><br /> 
+          <button onClick={this.addTask}>
+            SUBMIT!
+          </button>
+          <br /> 
+        </form> 
+      </div>  
+    )
+  }
+}
+
+export default CustomTask
